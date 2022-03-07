c7d13b3753ec9ed1a0244ee07011fb3738628485
Abdullah-Rehman786 <Air711@live.com>
2022-03-07 14:54:53 -0600

    lesson 1 exercises part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My favorite photos';
+  image1 = '../assets/Milky-Way-in-Antarctica.jpg';
+  image2 = `../assets/camp.jpg`;
+  image3 = '../assets/CalvinandHobbes.jpg';
 
   constructor() { }
 
